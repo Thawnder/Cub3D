afa85e7a39d6e9b2db0f914feb2c1b6113625234
Thawnder <realthawnder@outlook.fr>
2023-11-14 15:57:50 +0100

    main

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldeville <ldeville@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bpleutin <bpleutin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 10:47:29 by ldeville          #+#    #+#             */
-/*   Updated: 2023/11/14 10:55:47 by ldeville         ###   ########.fr       */
+/*   Updated: 2023/11/14 15:44:04 by bpleutin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,18 @@ int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
+	t_game	*game;
 
-	ft_printf("Salut !\n");
-	
+	game = ft_calloc(sizeof(t_game), 1);
+	game->mlx = mlx_init();
+	if (game->mlx == NULL)
+		print_error("Mlx is null.\n");
+	/*
+	after parsing
+		*/
+	if (game->win == NULL)
+		print_error("Window is null.\n");
+	mlx_key_hook(game->win, 2, &key_press, game);
+	mlx_key_hook(game->win, 3, &key_release, game);
 	return (0);
 }
