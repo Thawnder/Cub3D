de1c7de92f0c8fcac710cf48eecf807872c45480
Lucas Deville <ldeville@c2r7p5.42perpignan.fr>
2023-11-14 11:24:48 +0100

    Fixed minilibx

--- includes/common.h
+++ includes/common.h
@@ -6,13 +6,14 @@
 /*   By: ldeville <ldeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 10:46:35 by ldeville          #+#    #+#             */
-/*   Updated: 2023/11/14 11:01:43 by ldeville         ###   ########.fr       */
+/*   Updated: 2023/11/14 11:24:10 by ldeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMON_H
-#define COMMON_H
+# define COMMON_H
 
-# include "../includes/Libft/libft.h"
+# include "Libft/libft.h"
+# include "minilibx-linux/libft.h"
 
 #endif
